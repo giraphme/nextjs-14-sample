ae90931d0ef3dbd95f670f0116a384e8f3a0831a
TamakiMaeda <giraph.me@gmail.com>
2023-10-29 21:05:38 +0900

    add small sample for login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,94 @@
+import { css } from "@/styled-system/css";
+import { PropsWithChildren } from "react";
+
 export default function Home() {
-  return <main>top page</main>;
+  return (
+    <main>
+      <form
+        className={css({
+          w: "breakpoint-sm",
+          mx: "auto",
+          my: 10,
+          bg: "gray.100",
+          p: 10,
+          rounded: "lg",
+        })}
+      >
+        <h1
+          className={css({
+            fontSize: "2xl",
+            fontWeight: "bolder",
+            textAlign: "center",
+          })}
+        >
+          LOGIN
+        </h1>
+
+        <FormField label="メールアドレス">
+          <Input type="text" placeholder="sample@example.com" />
+        </FormField>
+
+        <FormField label="パスワード">
+          <Input type="password" />
+        </FormField>
+
+        <div
+          className={css({
+            display: "flex",
+            justifyContent: "flex-end",
+            mt: "10",
+          })}
+        >
+          <Button type="submit">ログイン</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
+
+function FormField({
+  label,
+  children,
+}: PropsWithChildren<{ label: React.ReactNode }>) {
+  return (
+    <label className={css({ mt: 5, display: "block" })}>
+      <div className={css({ fontWeight: "bolder" })}>{label}</div>
+      {children}
+    </label>
+  );
+}
+
+function Input(props: React.ComponentProps<"input">) {
+  return (
+    <input
+      className={css({
+        display: "block",
+        mt: "2",
+        w: "100%",
+        px: "6",
+        py: "3",
+        rounded: "md",
+      })}
+      {...props}
+    />
+  );
+}
+
+function Button(props: React.ComponentProps<"button">) {
+  return (
+    <button
+      className={css({
+        color: "white",
+        bg: "blue.500",
+        fontWeight: "bolder",
+        px: "4",
+        py: "2",
+        rounded: "lg",
+        fontSize: "lg",
+      })}
+      {...props}
+    >
+      ログイン
+    </button>
+  );
 }
