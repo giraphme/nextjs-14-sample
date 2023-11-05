a225e6260905dadb77c6ea3dc4f9564282bd9c88
TamakiMaeda <giraph.me@gmail.com>
2023-11-05 21:37:50 +0900

    add navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <main>
       <Link href="/examples/with-jsx">JSX を返す</Link>
+      <Link href="/examples/error-handling">
+        パスワードが間違っている場合に例外を throw する
+      </Link>
+      <Link href="/examples/with-neverthrow">
+        neverthrow を使って Result タイプを返す（動かない）
+      </Link>
+      <Link href="/examples/with-result-type">
+        自作の Result タイプを使用する
+      </Link>
     </main>
   );
 }
